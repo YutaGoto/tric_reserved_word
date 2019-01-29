ecc940c651931c37d2a697afd752a33da5f999d7
YutaGoto <you.goto.510@gmail.com>
2019-01-30 01:10:53 +0900

    add lint and my answer

--- tric.rb
+++ tric.rb
@@ -1,11 +1,65 @@
+unless
+  def class
+    super
+    true or return defined? next
+    break while
+      begin
+        __LINE__
+        undef
+        do end
+      rescue then
+        retry else
+        undef module
+        nil
+      ensure
+        until false
+          case if yield
+            self and redo
+            elsif not __FILE__
+            alias END in
+        end
+        when __ENCODING__
+      end
+  end
+end
 alias BEGIN
-  for unless
-    def class
-      super
-      true or return   defined? next
+for end
+
+p "---"
+
+alias    BEGIN    for      unless   def      class
+super    true     or       return   defined? next
 break    while    begin    undef    do       end
 rescue   then     retry    else     undef    module
 nil      ensure   case     if       yield    __LINE__
 self     and      redo     elsif    not      __FILE__
 alias    END      in       end      when     __ENCODING__
 end      until    false    end
+
+p "---"
+
+def class
+  self
+  yield
+  if false and retry
+    undef module
+  elsif not __FILE__
+    alias END in
+    case
+      while __LINE__
+        until true or return defined? next
+          redo
+        end
+        break
+      end
+    when __ENCODING__
+    else
+    end
+  end
+  begin
+  rescue
+  ensure
+  end
+end
+
+alias BEGIN class
